2866f43c1d4f4ce9afa6637ef9a8c5483829ac4c
B22 -user <asglazunova@gmail.com>
2021-05-27 21:36:30 -0400

    add Address changes 2

--- src/day47_constructors/Address.java
+++ src/day47_constructors/Address.java
@@ -6,6 +6,24 @@ public class Address {
     private String state;
     private String zipCode;
     private String country = "USA";
+//construction
+    public Address() {
+        System.out.println("Address constructor");
+        street = "123 unknown st";
+        city = "unknown";
+        state = "unknown";
+        zipCode= "00000";
+    }
+    //second constructor , overloaded constructor -> provides shortcut to initialize variables
+    // in same statement : ex : Address ad = new Address("123 java st","Boston","MA","43212");
+public Address(String street, String city, String state, String zipCode){
+        this.street = street ;
+        setStreet(street);  //reuse the code in the setter method
+    this.city = city;
+    this.state = state;
+    this.zipCode = zipCode;
+
+}
 
     @Override
     public String toString() {
@@ -18,9 +36,13 @@ public class Address {
     }
 
     public void setStreet(String street) {
-        this.street = street;
+        if (street.isEmpty() || street.length() > 50) {
+            System.out.println("Error : Invalid street");
+            //System.exit(0);
+        } else {
+            this.street = street;
+        }
     }
-
     public String getCity() {
         return city;
     }
--- src/day47_constructors/AddressAssigning.java
+++ src/day47_constructors/AddressAssigning.java
@@ -1,5 +1,29 @@
 package day47_constructors;
 
 public class AddressAssigning {
+    public static void main(String[] args) {
+        Address cybertekAddress = new Address();
+        cybertekAddress.setStreet("524 Gwynnwest Rd");
+        cybertekAddress.setCity("Baltimore");
+        cybertekAddress.setState("MD");
+        cybertekAddress.setZipCode("21136");
+//topic for read JAVA reflection API--> never used for us
+        System.out.println("CybertekSchool address: "+ cybertekAddress.toString());
+        cybertekAddress.setStreet("235 Phillip St");
+        System.out.println("Address after update: " + cybertekAddress);
+        System.out.println("street info: " + cybertekAddress.getStreet());
+
+        Address newAddress = new Address();
+        newAddress.setStreet("524 Gwynnwest Rd ");
+        newAddress.setCity("McLean");
+        newAddress.setZipCode("21220");
+        newAddress.setState("MD");
+        System.out.println(newAddress.toString());
+
+
+        Address papaJon = new Address("38 Main St", "Reisterstown", "MD", "21136");
+        System.out.println("Papa JOhn pizza = " + papaJon.toString());
+
+    }
 
 }
