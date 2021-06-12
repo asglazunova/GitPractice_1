125c4802fa08e7d2d5f7666410b58e642b2ea867
B22 -user <asglazunova@gmail.com>
2021-06-12 17:11:56 -0400

    super()- parent class constructor

--- src/day52_inheritance/AppObjects.java
+++ src/day52_inheritance/AppObjects.java
@@ -3,15 +3,18 @@ package day52_inheritance;
 public class AppObjects {
     public static void main(String[] args) {
         MobileApp mobileApp = new MobileApp();
-        mobileApp.name = "YouTube";
+        mobileApp.setName("Youtube");
         mobileApp.UseTheApp(10);
+        mobileApp.download();
 
         Instagram instagram = new Instagram();
-        mobileApp.name ="Instagram";
+        mobileApp.setName("Instagram");
         instagram.UseTheApp(20);
+        instagram.download();
 
         Discord b22Discord  = new Discord();
-        b22Discord.name  = "Discord";
+        b22Discord.setName("Discord");
         b22Discord.UseTheApp(100);
+        b22Discord.download();
     }
 }
--- src/day52_inheritance/Discord.java
+++ src/day52_inheritance/Discord.java
@@ -4,6 +4,18 @@ public class Discord extends MobileApp{
     public void chat (String someone){
         System.out.println("Chatting with " + someone);
     }
+    /**
+     * override download method
+     */
+
+    @Override
+    protected boolean download() {
+        System.out.println("Download Discord 0.0.23 from App Store ");
+        return true;
+    }
+
+
+
 
     @Override
     public void UseTheApp(int minutes){
