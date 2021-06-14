6b96af07e0ad67ac09f2ba14170c1fd367179cec
B22 -user <asglazunova@gmail.com>
2021-06-13 22:35:17 -0400

    added static variable

--- src/day53_inheritance/tesla/ElectricCar.java
+++ src/day53_inheritance/tesla/ElectricCar.java
@@ -1,12 +1,38 @@
-package day53_inheritance;
+package day53_inheritance.tesla;
 
-public class ElectricCar {
+public class ElectricCar { //template blue print
+
     private String make;
     private String model;
     private double price;
     private int year;
     private int range;
+    private static int count; // all object will share this variable
 
+    public ElectricCar(String make, String model, double price, int year, int range) {
+        setMake(make); //reuse the code in the setter condition
+        this.model = model;
+        this.price = price;
+        this.year = year;
+        this.range = range;
+        count ++; // increase count by 1 - everytime new car is created
+
+    }
+
+    public static int getCount() {
+        return count;
+    }
+
+    @Override
+    public String toString() {
+        return "ElectricCar{" +
+                "make='" + make + '\'' +
+                ", model='" + model + '\'' +
+                ", price=" + price +
+                ", year=" + year +
+                ", range=" + range +
+                '}';
+    }
 
     // model3.drive(34);
     protected void drive(int miles ) {
@@ -22,7 +48,11 @@ public class ElectricCar {
     }
 
     public void setMake(String make) {
-        this.make = make;
+        if (make.isEmpty()) {
+            System.out.println("ERROR : Make cannot be blank ");
+        } else {
+            this.make = make;
+        }
     }
 
     public String getModel() {
